aec7b7ed7efdb06f722e63ab17261110ec23492a
Hugues Collet <hc.pro@outlook.fr>
2023-09-12 17:00:52 +0200

    Projet Kasa

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,59 @@
 import Footer from "../components/Footer";
 import ArrowDown from "../assets/img/ArrowDown.png";
 import Montagnes from "../assets/img/ImageAbout.png";
+import { useState, useRef } from "react";
 
 function About() {
+  const [openFiability, setOpenFiability] = useState(false);
+  const [openRespect, setOpenRespect] = useState(false);
+  const [openService, setOpenService] = useState(false);
+  const [openSecurity, setOpenSecurity] = useState(false);
+
+  const fiability = useRef();
+  const respect = useRef();
+  const service = useRef();
+  const security = useRef();
+
+  function handleFiabilityOpen() {
+    setOpenFiability(!openFiability);
+    if (!openFiability) {
+      fiability.current.classList.add("open");
+      fiability.current.classList.remove("close");
+    } else {
+      fiability.current.classList.remove("open");
+      fiability.current.classList.add("close");
+    }
+  }
+  function handleRespectOpen() {
+    setOpenRespect(!openRespect);
+    if (!openRespect) {
+      respect.current.classList.add("open");
+      respect.current.classList.remove("close");
+    } else {
+      respect.current.classList.remove("open");
+      respect.current.classList.add("close");
+    }
+  }
+  function handleServiceOpen() {
+    setOpenService(!openService);
+    if (!openService) {
+      service.current.classList.add("open");
+      service.current.classList.remove("close");
+    } else {
+      service.current.classList.remove("open");
+      service.current.classList.add("close");
+    }
+  }
+  function handleSecurityOpen() {
+    setOpenSecurity(!openSecurity);
+    if (!openSecurity) {
+      security.current.classList.add("open");
+      security.current.classList.remove("close");
+    } else {
+      security.current.classList.remove("open");
+      security.current.classList.add("close");
+    }
+  }
   return (
     <div className="about-container">
       <header className="hero-image">
@@ -11,19 +62,43 @@ function About() {
       <div className="keyword-container">
         <div className="keywords-description">
           <p>Fiabilité</p>
-          <img src={ArrowDown} alt="Fleche bas" />
+          <img
+            src={ArrowDown}
+            alt="Fleche bas"
+            ref={fiability}
+            onClick={() => handleFiabilityOpen()}
+            className="close"
+          />
         </div>
         <div className="keywords-description">
           <p>Respect</p>
-          <img src={ArrowDown} alt="Fleche bas" />
+          <img
+            src={ArrowDown}
+            alt="Fleche bas"
+            ref={respect}
+            onClick={() => handleRespectOpen()}
+            className="close"
+          />
         </div>
         <div className="keywords-description">
           <p>Service</p>
-          <img src={ArrowDown} alt="Fleche bas" />
+          <img
+            src={ArrowDown}
+            alt="Fleche bas"
+            ref={service}
+            onClick={() => handleServiceOpen()}
+            className="close"
+          />
         </div>
         <div className="keywords-description">
           <p>Sécurité</p>
-          <img src={ArrowDown} alt="Fleche bas" />
+          <img
+            src={ArrowDown}
+            alt="Fleche bas"
+            ref={security}
+            onClick={() => handleSecurityOpen()}
+            className="close"
+          />
         </div>
       </div>
       <Footer />
--- src/pages/AppartementsSingle.jsx
+++ src/pages/AppartementsSingle.jsx
@@ -3,15 +3,51 @@ import Footer from "../components/Footer";
 import ArrowBackward from "../assets/img/ArrowBackward.png";
 import ArrowForward from "../assets/img/ArrowForward.png";
 import { useParams } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext, useState, useRef } from "react";
 import { getAPIresult } from "../theme/APIcontext";
 
 function AppartementsSingle() {
   const { id } = useParams();
   console.log(id);
   const { loading, error, data } = useContext(getAPIresult);
+
   const filterData = data?.filter((item) => item.id === id);
   console.log(filterData);
+
+  const rating = filterData[0]?.rating;
+  console.log(rating);
+  let starRating = [];
+  for (let index = 0; index < rating; index++) {
+    starRating.push(<i className="fa-solid fa-star"></i>);
+  }
+  console.log(starRating);
+
+  const [openDescription, setOpenDescription] = useState(false);
+  const [openEquipement, setOpenEquipement] = useState(false);
+
+  const description = useRef();
+  const equipement = useRef();
+
+  function handleDescriptionOpen() {
+    setOpenDescription(!openDescription);
+    if (!openDescription) {
+      description.current.classList.add("open");
+      description.current.classList.remove("close");
+    } else {
+      description.current.classList.remove("open");
+      description.current.classList.add("close");
+    }
+  }
+  function handleEquipementOpen() {
+    setOpenEquipement(!openEquipement);
+    if (!openEquipement) {
+      equipement.current.classList.add("open");
+      equipement.current.classList.remove("close");
+    } else {
+      equipement.current.classList.remove("open");
+      equipement.current.classList.add("close");
+    }
+  }
 
   return (
     <div>
@@ -35,8 +71,10 @@ function AppartementsSingle() {
               <h2>{filterData[0]?.title}</h2>
               <p>{filterData[0]?.location}</p>
               <div className="apartment-specification">
-                {filterData[0]?.tags.map((item) => (
-                  <button type="">{item}</button>
+                {filterData[0]?.tags.map((item, index) => (
+                  <button type="" key={index}>
+                    {item}
+                  </button>
                 ))}
               </div>
             </div>
@@ -48,11 +86,28 @@ function AppartementsSingle() {
                 </div>
               </div>
               <div className="rating">
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
-                <i className="fa-solid fa-star"></i>
+                <div className="star-rating">
+                  {starRating?.map((item, index) => (
+                    <span key={index}>{item}</span>
+                  ))}
+                </div>
+                <div className="star-rating grey">
+                  <span>
+                    <i className="fa-solid fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa-solid fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa-solid fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa-solid fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa-solid fa-star"></i>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
@@ -63,16 +118,30 @@ function AppartementsSingle() {
             <img
               src={ArrowDown}
               alt="Fleche vers le bas"
-              className="arrow-collapse"
+              className="arrow-collapse-description close"
+              ref={description}
+              onClick={() => handleDescriptionOpen()}
             />
+          </div>
+          <div className="description-collapse">
+            {filterData[0]?.description}
           </div>
           <div>
             <p>Equipements</p>
             <img
               src={ArrowDown}
               alt="Fleche vers le bas"
-              className="arrow-collapse"
+              className="arrow-collapse-equipement close"
+              ref={equipement}
+              onClick={() => handleEquipementOpen()}
             />
+          </div>
+          <div className="equipments-collapse">
+            {filterData[0]?.equipments?.map((item, index) => (
+              <p key={index} className="equipments-single">
+                {item}
+              </p>
+            ))}
           </div>
         </div>
       </div>
